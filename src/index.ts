b0d1584915f588a864a0cc86abfce6ac7dfc8ef9
zouariMustafa <spoward.dev.front@sossodigit.fr>
2020-10-17 19:52:52 +0100

    finished basic mikroOrm setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import {MikroORM} from '@mikro-orm/core'
+import { __prod__ } from './constants'
+import { Post } from './entities/Post'
+import microConfig from './mikro-orm.config'
 
 const main = async ()=>{
-    const orm = await MikroORM.init()
+    const orm = await MikroORM.init(microConfig)
+    await orm.getMigrator().up()
+    // const post = orm.em.create(Post, {title : 'first title'})
+    // await orm.em.persistAndFlush(post)
+    // const posts = await orm.em.find(Post , {})
+  
 }
+main().catch(err=>{console.log(err)})
